d916a5cba3447b18abafb2ab832c394617418287
Kaiuschy <112570674+Kaiuschy@users.noreply.github.com>
2023-03-17 11:09:00 -0300

    Update BuscarCep.cy.js

--- BuscarCep.cy.js
+++ BuscarCep.cy.js
@@ -1,7 +1,8 @@
 const faker = require('faker')
 
 describe('Busca de CEP', () => {
-  Cypress._.times(50, () => {
+  Cypress._.times(50, () => { // Escolher o número de vezes em que o teste irá repetir
+    
     it('Encontrar CEP válido', () => {
       cy.visit('https://buscacepinter.correios.com.br/app/endereco/index.php')
       
@@ -9,14 +10,13 @@ describe('Busca de CEP', () => {
       let cepGerado;
   
       while (!cepValidoEncontrado) {
-        cepGerado = faker.address.zipCode("#####-###")
+        cepGerado = faker.address.zipCode("#####-###") // Gerar CEP aleatório 
         cy.get('#endereco').clear().type(cepGerado)
         cy.get('#btn_pesquisar').click()
-        if (cy.get('#resultado-DNEC > thead > tr > [data-th="CEP"]').contains('CEP').should('be.visible')) {
+        if (cy.get('#resultado-DNEC > thead > tr > [data-th="CEP"]').contains('CEP').should('be.visible')) { //Valida se o CEP gerado foi encontrado 
           cepValidoEncontrado = true;
         }
       }
-  
       // Se chegou aqui, significa que um CEP válido foi encontrado
       cy.log(`CEP válido encontrado: ${cepGerado}`)
     })
